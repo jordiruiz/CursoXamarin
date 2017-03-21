53624c7ca5a068f5afd8951b515d15432a2d827b
jordiruizperez <jordi.ruiz@takeu.es>
2017-03-21 07:22:51 +0100

    add slecteditem

--- Modulo2_Cities/Modulo2_Cities/Modulo2_Cities/ViewModels/MainViewModel.cs
+++ Modulo2_Cities/Modulo2_Cities/Modulo2_Cities/ViewModels/MainViewModel.cs
@@ -12,6 +12,31 @@ namespace Modulo2_Cities.ViewModels
         public MainViewModel()
         {            
             Cities = CitiesService.GetCities();
-        }        
+        }
+
+        private City _ItemSelected;
+        public City objItemSelected
+        {
+            get
+            {
+                return _ItemSelected;
+            }
+            set
+            {
+                if (_ItemSelected != value)
+                {
+                    _ItemSelected = value;
+                    OnPropertyChanged("ItemSelected");
+
+                    City c = new City();
+                    c.Name = _ItemSelected.Name;
+                    c.Location = _ItemSelected.Location;
+                    c.Details = _ItemSelected.Details;
+                    c.Image = _ItemSelected.Image;
+                    Cities = new ObservableCollection<City> { c };
+                    Application.Current.MainPage.Navigation.PushAsync(new CityDetailsView3(Cities));
+                }
+            }
+        }
     }
 }
